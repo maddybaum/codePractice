f155ab0548322b8fabb5dcd0a8251844d281b8f3
maddybaum <madeline.baum94@gmail.com>
2023-03-14 20:46:28 -0500

    Practice

---
+++ Practice1.js
@@ -0,0 +1,116 @@
+//PRACTICE
+
+//Declare a variable called "name" and assign it your name 
+
+//Code here
+
+//Declare a variable called "age" and assign it your age
+
+//Code here
+
+//Declare a variable called sentence that says "Hello my name is Biggs and I am a hairless cat"
+
+//Code here
+
+//Declare a variable with your address and assign it your address (think about what kind of data type an address is)
+
+//Code here
+
+//Declare a variable called basketballTeams and make it a list of your 5 favorite basketball teams (what kind of data type is a list?)
+
+//Code here
+
+//Declare a variable called nums and include 5 of your favorite numbers (this is a list of your favorite numbers)
+
+//Code here
+
+//Declare a variable called tall and assign it the accurate boolean value
+
+//Code here
+
+//Declare a variable with the year you were born called birthYear
+
+//Code here
+
+//Declare a variable with your birthday (for month write the word "January"... what data type is this?)
+
+//Code here
+
+//Check if age is greater than 30. If age is greater than 30, console.log("You are old"). If age is equal to 30, console.log("Happy 30th")
+//and if age is younger than 30, console.log("You're young")
+
+//Code here
+
+//Create a variable called biggsSentence and set it equal to sentence, except turn the string from sentence into an array. Split each word into a separate item in the array
+
+//Code here
+
+//Reverse the order of the array called basketballTeams. Do not reverse the individual words, just put it in opposite order. Put this in a new array called reverseTeams
+
+//Code here
+
+//Create a loop to console.log the first 3 teams in your reverseTeams array
+
+//Code here
+
+//Check to see if the first item in the reverseTeams is "Knicks". If it is, console log "Go knicks" and if it's "Lakers" console log "poor lebron"
+//if it's Portland, console.log("Boo") and if it's anybody else console log "Bad taste"
+
+//Code here
+
+//Create a loop to countdown from 15. If the number is divisible by 3 (use %) then console log the number, if it's not, then do nothing
+
+//Code here
+
+//Check to see if birthYear is after 1990. If it is, then console log "millenial" if it's not then console.log "old"
+
+//Code here
+
+let pets = ["Rory", "Puppy", "Biggs", "Basil"]
+
+//Console log the length of the pets array
+
+//Code here
+
+//Check to see if the second item in the array is Patch. If it is, console log "Hi Patch". If it's not, console.log("Where's patch?")
+
+//Code here
+
+//Console log each item in the pets array
+
+//Code here
+
+//Reverse the order of every word in the pets array
+
+//Code here
+
+let stringToChange = "Hello, right now I am a string"
+
+//Turn this string into a new array called aPlus. Separate it at the letter a
+
+//Code here
+
+//In the pets array, start at Basil and console log each name in the order Basil, Biggs, Puppy, Rory. Use a loop to do this, and do not change the array
+
+//Code here
+
+//Create a variable called cutestCat and set it equal to the cat you think is the cutest using the pets array
+
+//Code here
+
+//Create a loop that counts down from 15. If a number is divisible by 5, console log it
+
+//Code here
+
+//Create a loop that counts from 0 to 20. If a number is divisible by 4, console log it
+
+//Code here
+
+//Create a loop that counts from 0 to 40. If a number is divisble by 4 and 5, console log it (REMEMBER HOW TO DO AND)
+
+//Code here
+
+//  CHALLENGE: (This is a very common interview question) 
+//Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'fizz'. If the number is divisible by 5, console.log 'buzz'. If the number is divisible by 5 & 3, console.log 'fizzbuzz'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+
+//Code here
